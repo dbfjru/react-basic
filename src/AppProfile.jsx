7c0e95c5011e4a3a7dfc58434ad37232d9b8cd88
dbfjru <dbfjru@gmail.com>
2025-07-13 21:03:27 +0900

    2-4. First Component - add button and event

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -3,8 +3,13 @@ import Profile from "./components/Profile.jsx";
 import Avatar from "./components/Avatar.jsx";
 
 const AppProfile = () => {
+    const handleClick = (event) => {
+        console.log(event);
+        alert("clicked1");
+    }
     return (
         <div>
+            <button onClick={handleClick}>button</button>
             <Avatar url={'https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'} isNew={true}/>
             <Profile image={'https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'}
                      name={'James Kim'}
