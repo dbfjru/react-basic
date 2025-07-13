3a2f5d7b008d32a5d80ca891e96edff5c35f4d71
dbfjru <dbfjru@gmail.com>
2025-07-13 19:44:15 +0900

    2-2. First Component - challenge new tag

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './css/profile.module.css'
 
-export default function Profile({image, name, title}) {
+export default function Profile({image, name, title, isNew}) {
     return (
         <div className={styles.profile}>
             <img className={styles.photo}
                  src={image}
                  alt="avatar"/>
+            {isNew && <span className={styles.tag}>NEW</span>}
             <h1>{name}</h1>
             <p>{title}</p>
         </div>
