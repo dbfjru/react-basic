df9b25b994817f874bbcad526563ad9702668bbf
dbfjru <dbfjru@gmail.com>
2025-07-13 19:21:57 +0900

    2. First Component - no props

---
+++ src/AppProfile.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Profile from "./components/Profile.jsx";
+
+const AppProfile = () => {
+    return (
+        <div>
+            <Profile/>
+            <Profile/>
+        </div>
+    );
+};
+
+export default AppProfile;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import AppJSX from "./AppJSX.jsx";
+import AppProfile from "./AppProfile.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppJSX/>
+    <AppProfile/>
   </StrictMode>
 )
